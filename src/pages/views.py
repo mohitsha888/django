3dbaee2e53cf7ec5564899c7644d9812c68583b4
Mohit Sharma <themohitsha@gmail.com>
2024-07-10 12:21:12 +0530

    Initial commit

---
+++ src/pages/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+        return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+        return render(request, "about.html", {})
+
+
+def blog_view(request, *args, **kwargs):
+    return render(request, "blog.html", {})
