d82d1aa9f25d0186723306061e4b27478c40e813
Mohit Sharma <themohitsha@gmail.com>
2024-07-11 18:29:13 +0530

    changes in templates

--- src/products/models.py
+++ src/products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
     summary     = models.TextField(default='This is cool!')
-    featured    = models.BooleanField() # null=True, default=True
+    featured    = models.BooleanField(default = True) # null=True, default=True
